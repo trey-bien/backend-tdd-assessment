3919b3aaaf3d925e5bf1dffcc670675160ce2dba
Trey Dickerson <treydickerson@gmail.com>
2020-07-17 15:47:46 -0600

    dev complete

--- echo.py
+++ echo.py
@@ -2,22 +2,40 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility."""
 
-__author__ = "???"
+__author__ = "Trey Dickerson"
 
-
+import argparse
 import sys
 
 
 def create_parser():
     """Returns an instance of argparse.ArgumentParser"""
-    # your code here
-    return
+    parser = argparse.ArgumentParser(description="Perform transformation on input text.")
+    parser.add_argument('text', help="text to be manipulated")
+    parser.add_argument('-t', '--title', action='store_true',
+                        help="convert text to titlecase")
+    parser.add_argument('-l', '--lower', action='store_true',
+                        help="convert text to lowercase")
+    parser.add_argument('-u', '--upper', action='store_true',
+                        help="convert text to uppercase")
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    # your code here
-    return
+    parser = create_parser()
+    ns = parser.parse_args(args)
+
+    msg = ns.text
+
+    if ns.upper:
+        msg = msg.upper()
+    if ns.lower:
+        msg = msg.lower()
+    if ns.title:
+        msg = msg.title()
+    print(msg)
+    return msg
 
 
 if __name__ == '__main__':
